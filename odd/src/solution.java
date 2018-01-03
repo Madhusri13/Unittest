bbba8c4319f4f6d2f595b7cb033f20a56be5824d
sankarpraveen02 <35062966+sankarpraveen02@users.noreply.github.com>
2018-01-03 22:30:43 +0530

    Update solution.java

--- odd/src/solution.java
+++ odd/src/solution.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class solution {
-
+//remote
 	public static void main(String[] args) throws IOException {
 	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	System.out.println("Enter total numbers");
