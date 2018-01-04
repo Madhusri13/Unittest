c3b425c2677a2c088bb8f1a2730a81490560c671
sankarpraveen02 <35062966+sankarpraveen02@users.noreply.github.com>
2018-01-04 15:08:52 +0530

    Update solution.java

--- odd/src/solution.java
+++ odd/src/solution.java
@@ -8,7 +8,7 @@ public class solution {
 	public static void main(String[] args) throws IOException {
 	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	System.out.println("Enter total numbers");
-	/*int k=Integer.valueOf(bf.readLine());
+	int k=Integer.valueOf(bf.readLine());
 	int[] a=new int[k];
 	int c=0;
 	for(int i=0;i<k;i++) {
@@ -16,12 +16,12 @@ public class solution {
 		if(a[i]%2==0) {
 			c++;
 		}
-	}*/
+	}
 	ArrayList<Integer> a = new ArrayList<Integer>();
 	a.add(1);
 	a.add(2);
 	System.out.println(a.get(0));
-	//System.out.println("Odd Number : "+(k-c)+"\nEven Numbers : "+c);
+	System.out.println("Odd Number : "+(k-c)+"\nEven Numbers : "+c);
 	}
 
 }
